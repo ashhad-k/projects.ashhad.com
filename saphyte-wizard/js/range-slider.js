0808eea46467b82846178b224c5919efcf00208e
Ashhad Khan <62101376+ashhad-k@users.noreply.github.com>
2022-04-23 23:28:47 +0500

    saphyte wizard

---
+++ saphyte-wizard/js/range-slider.js
@@ -0,0 +1,40 @@
+var sheet = document.createElement('style'),  
+  $rangeInput = $('.range input'),
+  prefs = ['webkit-slider-runnable-track', 'moz-range-track', 'ms-track'];
+
+document.body.appendChild(sheet);
+
+var getTrackStyle = function (el) {  
+  var curVal = el.value,
+      val = (curVal - 1) * 14.28,
+      style = '';
+  
+  // Set active label
+  $('.range-labels li').removeClass('active selected');
+  
+  var curLabel = $('.range-labels').find('li:nth-child(' + curVal + ')');
+  
+  curLabel.addClass('active selected');
+  curLabel.prevAll().addClass('selected');
+  
+  // Change background gradient
+  for (var i = 0; i < prefs.length; i++) {
+//    style += '.range {background: linear-gradient(to right, #a745e0 0%, #4daad4 ' + val + '%, #494c57 ' + val + '%, #494c57 100%)}';
+    style += '.range input::-' + prefs[i] + '{background: linear-gradient(to right, #a745e0 0%, #4daad4 ' + val + '%, #494c57 ' + val + '%, #494c57 100%)}';
+  }
+
+  return style;
+}
+
+$rangeInput.on('input', function () {
+  sheet.textContent = getTrackStyle(this);
+});
+
+// Change input value on label click
+$('.range-labels li').on('click', function () {
+  var index = $(this).index();
+  
+  $rangeInput.val(index + 1).trigger('input');
+  
+});
+
