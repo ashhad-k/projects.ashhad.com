34f34c57e36316e7cff787e27529b2f07d0327db
Ashhad Khan <ashhad4u@gmail.com>
2024-06-08 21:54:15 +0500

    contact page update

--- loyica/assets/js/loyica-contact.js
+++ loyica/assets/js/loyica-contact.js
@@ -1,1 +1,13 @@
-//
+ document.addEventListener("DOMContentLoaded", function() {
+    const modal = document.getElementById('modal1');
+
+    modal.addEventListener('shown.bs.modal', function () {
+      // Disable default scrolling
+      document.body.style.overflow = 'hidden';
+    });
+
+    modal.addEventListener('hidden.bs.modal', function () {
+      // Enable default scrolling
+      document.body.style.overflow = 'auto';
+    });
+  });
