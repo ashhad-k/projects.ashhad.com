5322ed96e8d3f1b26c7b4b3c387fb8bbbd8124de
Ashhad Khan <ashhad4u@gmail.com>
2024-06-20 22:35:50 +0500

    afghan-consulate1

---
+++ afghan-consulate/js/scripts.js
@@ -0,0 +1,37 @@
+AOS.init({
+    offset: 100,
+    duration: 500,
+    easing: 'ease-in-out',
+    delay: 100,
+});
+
+AOS.init({
+  disable: function() {
+    var maxWidth = 1024;
+    return window.innerWidth < maxWidth;
+  }
+});
+
+
+
+//
+const myCarouselElement = document.querySelector('#carousel1')
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false
+})
+
+
+// apply class when user scrolled navigation
+$(document).ready(function() {
+  var prevScrollPos = $(window).scrollTop();
+  $(window).scroll(function() {
+    var currentScrollPos = $(window).scrollTop();
+    if (prevScrollPos < currentScrollPos) {
+      $("#nav1").addClass("scrolled-down");
+    } else {
+      $("#nav1").removeClass("scrolled-down");
+    }
+    prevScrollPos = currentScrollPos;
+  });
+});
